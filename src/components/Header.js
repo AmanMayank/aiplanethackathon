932de53681908e63bb2244ea137587f39e78e21c
AmanMayank <aman.msingh87@gmail.com>
2024-09-05 19:25:53 +0530

    HeaderDone

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="min-w-[300px] w-full sm:h-12 md:h-16 bg-white ">
+      <div className="sm:py-[2px] md:py-3 ml-5 sm:ml-10 md:ml-20">
+        <img src="./assets/icons/Group 1000002532.svg" alt="logo" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
