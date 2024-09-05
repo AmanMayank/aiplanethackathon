0f438181029ef7eac5c765a27d64e4f1ad2585f1
AmanMayank <aman.msingh87@gmail.com>
2024-09-05 19:04:44 +0530

    InitialSetup

---
+++ src/components/ChallengeCards.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChallengeCards = () => {
+  return <div>ChallengeCards</div>;
+};
+
+export default ChallengeCards;
---
+++ src/components/Homepage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import HeroCard from "./HeroCard";
+import CardDisplay from "./CardDisplay";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Header />
+      <HeroCard />
+      <CardDisplay />
+    </div>
+  );
+};
+
+export default Homepage;
